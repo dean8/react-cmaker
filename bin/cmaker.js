33346ed2ed0a22862995974926f510f5ab8f8cc1
DeanHan <826554003@qq.com>
2016-11-07 17:46:35 +0800

    change js

--- bin/cmaker.js
+++ bin/cmaker.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 var program = require('commander');
 var createComponent = require('../lib/createComponent.js');
 
@@ -15,6 +14,9 @@ program
   .parse(process.argv)
 
 if (program.name) {
+  if (program.name.length === 0) {
+    return console.error("please enter component name!");
+  }
   console.log('You will create components %s with %s', program.name, program.stype);
   createComponent(program.name, program.stype, program.template);
 }
